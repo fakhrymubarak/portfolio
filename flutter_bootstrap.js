521d76a87b3228e807df45d1b1e7636e4fd05365
fakhrymubarak <fakhrymubarak@users.noreply.github.com>
2025-06-22 07:56:20 +0000

    deploy: 9f79f2a4836ea77c7c4854e2b0e2cc7bf207b79f

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3669687251"
+    serviceWorkerVersion: "4047913790"
   }
 });
